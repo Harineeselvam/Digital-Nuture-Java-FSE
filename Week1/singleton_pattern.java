53bc745d1d4c16d5e06097ea8a0c1076a85ca837
Harineeselvam <189009683+Harineeselvam@users.noreply.github.com>
2026-06-17 22:34:52 +0530

    Refactor singleton_pattern to Logger class
    Replaced singleton_pattern class with Logger class implementation. Updated main method to demonstrate Logger functionality.

--- Week1/singleton_pattern.java
+++ Week1/singleton_pattern.java
@@ -1,27 +1,26 @@
 package week1;
-
+class Logger {
+    private static Logger instance;
+    private Logger() {
+        System.out.println("Logger Initialized");
+    }
+    public static Logger getInstance() {
+        if(instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
 public class singleton_pattern {
-	private static singleton_pattern instance;
-	private singleton_pattern() {
-		System.out.println("Datatbase connection created");
-	}
-public static singleton_pattern getInstance() {
-	if(instance == null) {
-		instance = new singleton_pattern();
-	}
-	return instance;
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        logger1.log("Application Started");
+        logger2.log("User Logged In");
+        System.out.println(logger1 == logger2);
+    }
 }
-public void connect()
-{
-	System.out.println("Connected Succcessfully");
-}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		singleton_pattern db1 =  singleton_pattern.getInstance();
-		singleton_pattern db2 =  singleton_pattern.getInstance();
-		db1.connect();
-		System.out.println(db1==db2);
 
-	}
-
-}
